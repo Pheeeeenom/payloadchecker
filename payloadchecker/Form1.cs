12a97c4bb74db9d893b87c7042ffdb7c97fafa36
Pheeeeenom <emdazer@gmail.com>
2022-01-01 17:47:00 -0500

    check if dialog ok, dispose of resources

--- payloadchecker/Form1.cs
+++ payloadchecker/Form1.cs
@@ -16,7 +16,7 @@ namespace payloadchecker
         {
             InitializeComponent();
         }
-        byte[] payload_v2 = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         byte[] payload_v2 = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                         0x59, 0xdd, 0xb4, 0x1f, 0x41, 0x28, 0xff, 0x4e, 0x70, 0xd2, 0x48, 0xcc, 0x1f, 0x7a, 0xd6, 0xab };
 		 byte[] payload_v1 = new byte[]{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
                                         0x12, 0x8f, 0xfe, 0x1d, 0x86, 0xdc, 0x44, 0x2a, 0xad, 0x49, 0xe7, 0x24, 0x1a, 0x76, 0xc6, 0x6a};
@@ -25,25 +25,35 @@ namespace payloadchecker
         {
             OpenFileDialog ofd = new OpenFileDialog();
 
-            ofd.ShowDialog();
-            FileStream stream = File.OpenRead(ofd.FileName);
+          
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                ofd.Dispose();
+                FileStream stream = File.OpenRead(ofd.FileName);
 
-            byte[] BOOT0 = new byte[32];
-            stream.Position = 0x3F0000;
-            stream.Read(BOOT0, 0, 0x20);
+                byte[] BOOT0 = new byte[32];
+
+                stream.Position = 0x3F0000;
+                stream.Read(BOOT0, 0, 0x20);
+
+                if (BOOT0.SequenceEqual(payload_v1))
+                {
+                    MessageBox.Show("Spacecraft 0.1.0 Release\nDo not use this chip on an OLED");
+                }
+                else if (BOOT0.SequenceEqual(payload_v2))
+                {
+                    MessageBox.Show("Spacecraft 0.2.0 Release\nYou can use this chip on an OLED");
+                }
+                else
+                {
+                    MessageBox.Show("Unknown Payload. Please contact the developer");
+
+                }
+            }
+         
             
 
-            if (BOOT0.SequenceEqual(payload_v1))
-            {
-                MessageBox.Show("Spacecraft 0.1.0 Release\nDo not use this chip on an OLED");
-            } else if (BOOT0.SequenceEqual(payload_v2))
-            {
-                MessageBox.Show("Spacecraft 0.2.0 Release\nYou can use this chip on an OLED");
-            } else
-            {
-                MessageBox.Show("Unknown Payload. Please contact the developer");
-
-            }
+         
         }
 
     }
