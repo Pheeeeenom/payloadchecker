d18725f4e53125c87456563efa218e24a5bacc99
Pheeeeenom <emdazer@gmail.com>
2022-01-09 09:29:45 -0500

    latest spacecraft-mod support

--- payloadchecker/Form1.cs
+++ payloadchecker/Form1.cs
@@ -20,8 +20,8 @@ namespace payloadchecker
         }
         uint payload_v1 = 0xd90f1907, payload_v2 = 0x3d36ffac, gateway_payload = 0x3b25Bab4, payload_v2_improved_sd_BOOT2OFW = 0xf67893ad, stock_oled = 0x8bdd4e3e, hwliteg3 = 0x13e8eea5;
 
-        uint spacecraftv1 = 0x5104d6f4, spacecraftv2 = 0x5074125e;
-        uint spacecraftv1_len = 0xF9C0, spacecraftv2_len = 0xFC60;
+        uint spacecraftv1 = 0x5104d6f4, spacecraftv2 = 0x5074125e, spacecraftv2_mod= 0x3a560dc4;
+        uint spacecraftv1_len = 0xF9C0, spacecraftv2_len = 0xFC60, spacecraftv2_mod_len = 0xCB20;
         private void button1_Click(object sender, EventArgs e)
         {
 	
@@ -57,8 +57,20 @@ namespace payloadchecker
                     MessageBox.Show("Do use on an OLED", "");
                 }
                 #endregion
+                #region spacecraft v2 mod check
+                byte[] BOOT0_2nd_Mod = new byte[0xCB20];
 
-                if((crc32BOOT0 != spacecraftv1) &&(crc32BOOT0_ != spacecraftv2))
+                stream.Position = 0x3F0000;
+                stream.Read(BOOT0_2nd_Mod, 0, 0xCB20);
+
+                uint crc32BOOT0_Mod = Crc32Algorithm.Compute(BOOT0_2nd_Mod);
+
+                if (crc32BOOT0_Mod == spacecraftv2_mod)
+                {
+                    MessageBox.Show("Do use on an OLED", "");
+                }
+                #endregion
+                if ((crc32BOOT0 != spacecraftv1) && (crc32BOOT0_ != spacecraftv2) && (crc32BOOT0_Mod != spacecraftv2_mod))
                 {
                     MessageBox.Show("Unknown Payload/SX Payload?", "");
                 }
